6939baaca51aba95be9f7b95a15d049d6aa5e0a3
sneheilsaxena <sneheilsaxena@gmail.com>
2020-02-14 18:46:12 -0800

    Fixed some bugs.

--- src/main/Database.java
+++ src/main/Database.java
@@ -1,14 +1,16 @@
 package main;
 
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
 
 public class Database {
 
     private HashMap<Integer, Vehicle> vehicles = new HashMap<>();
 
     public void addEntry(int ID, int year, String make, String model) {
+        if (vehicles.containsKey(ID)) {
+            System.out.println("Record with that ID already exists in the database.");
+            return;
+        }
         Vehicle vehicle = new Vehicle();
         vehicle.setID(ID);
         vehicle.setYear(year);
@@ -26,21 +28,27 @@ public class Database {
     }
 
     public void retrieveAllEntries() {
-        // Getting an iterator
-        Iterator vehicleIterator = vehicles.entrySet().iterator();
+
+        if (vehicles.isEmpty()) {
+            System.out.println("No entries in the database.");
+            return;
+        }
 
         // Iterate through the hashmap
         System.out.println("List of vehicles (in no particular order):");
 
-        while (vehicleIterator.hasNext()) {
-            Map.Entry mapElement = (Map.Entry) vehicleIterator.next();
-            System.out.println(mapElement.getValue());
+        for (Vehicle entry : vehicles.values()) {
+            System.out.println(entry);
         }
 
         System.out.println();
     }
 
-    public void updateEntry (int ID, int year, String make, String model) {
+    public void updateEntry(int ID, int year, String make, String model) {
+        if (!vehicles.containsKey(ID)) {
+            System.out.println("No record with that ID exists in the database. Please try again.\n");
+            return;
+        }
         Vehicle vehicle = new Vehicle();
         vehicle.setID(ID);
         vehicle.setYear(year);
@@ -49,11 +57,16 @@ public class Database {
         vehicles.put(ID, vehicle);
     }
 
-    public void deleteEntry (int ID) {
+    public void deleteEntry(int ID) {
+        if (!vehicles.containsKey(ID)) {
+            System.out.println("No record with that ID exists in the database. Please try again.\n");
+            return;
+        }
         vehicles.remove(ID);
     }
 
     Database() {
+
     }
 
 }
--- src/main/VehicleDatabase.java
+++ src/main/VehicleDatabase.java
@@ -4,26 +4,36 @@ import java.util.Scanner;
 
 public class VehicleDatabase {
 
-    public static void printAddOptions () {
+    public static void printAddOptions() {
         System.out.println("Enter the ID, year, make, and model by typing in the value and pressing enter" +
                 " (ID & year must be only digits).");
     }
 
-    public static void printROptions () {
-        System.out.println("You can either read a specific record based on ID or read all");
+    public static void printRetrieveOptions() {
+        System.out.println("Enter the ID for the item you wish to retrieve:");
+    }
+
+    public static void printUpdateOptions() {
+        System.out.println("Enter the ID of the item you want to change and then the year, make," +
+                " and model by typing in the value and pressing enter (ID & year must be only digits).");
+    }
+
+    public static void printDeleteOptions() {
+        System.out.println("Enter vehicle ID to be deleted:");
     }
 
     private static void printMenuOptions() {
         System.out.println("You can add, retrieve, update, and delete entries:");
-        System.out.println("1. Add - A\n2. Retrieve All- RA\n3. Retrieve by ID - R\n4. Update - U\n5. Delete - D\n6. Exit - any other key");
+        System.out.println("1. Add - A\n2. Retrieve All- RA\n3. Retrieve by ID - R\n4. Update - U\n" +
+                "5. Delete - D\n6. Exit - any other key");
         System.out.println("Type in your choice and press enter (options are case sensitive).");
     }
 
-    private static boolean verifyChoice (String inp) {
+    private static boolean verifyChoice(String inp) {
         return inp.equals("A") || inp.equals("RA") || inp.equals("R") || inp.equals("U") || inp.equals("D");
     }
 
-    public static void main (String [] args) {
+    public static void main(String[] args) {
 
         // welcome message
         System.out.println("Welcome to the Mitchell vehicle database!");
@@ -35,42 +45,48 @@ public class VehicleDatabase {
         Database db = new Database();
 
         while (verifyChoice(inp)) {
-            if (inp.equals("A")) {
-                printAddOptions();
-                int ID = in.nextInt();
-                int year = in.nextInt();
-                in.nextLine();
-                String make = in.nextLine();
-                String model = in.nextLine();
-                db.addEntry(ID, year, make, model);
-            }
-            else if (inp.equals("RA")) {
-                db.retrieveAllEntries();
-            }
-            else if (inp.equals("R")) {
-                System.out.println("Enter the ID for the item you wish to retrieve:");
-                int ID = in.nextInt();
-                in.nextLine();
-                db.retrieveSingleEntry(ID);
-            }
-            else if (inp.equals("U")) {
-                printAddOptions();
-                int ID = in.nextInt();
-                int year = in.nextInt();
-                in.nextLine();
-                String make = in.nextLine();
-                String model = in.nextLine();
-                db.updateEntry(ID, year, make, model);
-                // check case where ID already exists
-            }
-            else if (inp.equals("D")) {
-                System.out.println("Enter vehicle ID to be deleted:");
-                int ID = in.nextInt();
-                in.nextLine();
-                db.deleteEntry(ID);
-            }
-            else {
-                break;
+            switch (inp) {
+                case "A": {
+                    printAddOptions();
+                    int ID = in.nextInt();
+                    int year = in.nextInt();
+                    in.nextLine();
+                    String make = in.nextLine();
+                    String model = in.nextLine();
+                    db.addEntry(ID, year, make, model);
+                    break;
+                }
+                case "RA": {
+                    db.retrieveAllEntries();
+                    break;
+                }
+                case "R": {
+                    printRetrieveOptions();
+                    int ID = in.nextInt();
+                    in.nextLine();
+                    db.retrieveSingleEntry(ID);
+                    break;
+                }
+                case "U": {
+                    printUpdateOptions();
+                    int ID = in.nextInt();
+                    int year = in.nextInt();
+                    in.nextLine();
+                    String make = in.nextLine();
+                    String model = in.nextLine();
+                    db.updateEntry(ID, year, make, model);
+                    break;
+                }
+                case "D": {
+                    printDeleteOptions();
+                    int ID = in.nextInt();
+                    in.nextLine();
+                    db.deleteEntry(ID);
+                    break;
+                }
+                default: {
+                    break;
+                }
             }
             printMenuOptions();
             inp = in.nextLine();
@@ -78,7 +94,7 @@ public class VehicleDatabase {
 
         db = null;
         System.out.println("Thank you for using the Mitchell vehicle database!");
-        System.out.println("Don't forget to rate us on the App Store! We value your feedback and are" +
-                "continuously looking to improve, so every bit helps :)");
+        System.out.println("Don't forget to rate/review us on the App Store! We value your feedback and are" +
+                " continuously looking to improve, so every bit helps :)");
     }
 }
